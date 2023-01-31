a261a2328b26fe1e16b1ea756d9442a80837de3a
ajeer333 <ajeer333@gmail.com>
2023-01-31 13:13:15 +0100

    commit

--- day-one/src/App.js
+++ day-one/src/App.js
@@ -8,10 +8,10 @@ function App() {
     <div className="App">
      
       <header className="App-header">
-      <ButtonComponent label="Start" CustomClass="my-btn-top" />
-      <ImageComponent myCssClass="image-top" srcFile="https://images.unsplash.com/photo-1674905682134-496ca5a4862b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="pink" />
+      <ButtonComponent label="Start" CustomClass="my-btn-top my-btn" />
+      <ImageComponent myCssClass="image-top image" srcFile="https://images.unsplash.com/photo-1674905682134-496ca5a4862b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="pink" />
         <img src={logo} className="App-logo" alt="logo" />
-      <ImageComponent myCssClass="image-bottom" srcFile="https://plus.unsplash.com/premium_photo-1670333242665-30b397067c37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="donuts" />
+      <ImageComponent myCssClass="image-bottom image" srcFile="https://images.unsplash.com/photo-1544194215-541c2d3561a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="pink" />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
@@ -24,7 +24,7 @@ function App() {
         >
           Learn React
         </a> */}
-        <ButtonComponent label="End" CustomClass="my-btn-bottom" />
+        <ButtonComponent label="End" CustomClass="my-btn-bottom my-btn" />
       </header>
    
     </div>
